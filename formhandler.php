474b0f2b5e3936c3f30519a2d34eb3815cdb5a51
susanachieng <sachieng137@gmail.com>
2024-11-01 20:04:23 +0300

    new changes

---
+++ formhandler.php
@@ -0,0 +1,15 @@
+<?<php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'sachieng137@gmail.com';
+
+$email_subject= 'New Form Submission';
+
+$email_body = "User Name: $name./n"
+
+
+?>
